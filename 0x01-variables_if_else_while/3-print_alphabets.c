2508a26714ad9ee38b893f1a5da4cebe0d5d22b4
Jaredglass3 <Jaredglass352@gmail.com>
2022-09-19 10:31:03 -0700

    If, Else, When project 3.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include
+
+/**
+* main - Prints the Alphabet in lowercase and uppercase letters
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+char c;
+
+for(c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+
+putchar('\n');
+
+return (0)
+}
