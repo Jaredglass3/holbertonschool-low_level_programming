3550d5f489c28ffca62fa82ed8baf1e2cbe65166
Jaredglass3 <Jaredglass352@gmail.com>
2022-09-19 10:35:41 -0700

    If, Else, When project 3.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 {
 char c;
 
-for(c = 'a'; c <= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 putchar(c);
 }
@@ -21,5 +21,5 @@ putchar(c);
 
 putchar('\n');
 
-return (0)
+return (0);
 }
